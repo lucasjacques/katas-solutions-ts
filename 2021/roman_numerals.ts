b366b14f73f476b5c3dbf989970f03557ff3da85
Lucas Jacques <lucas.jacs@gmail.com>
2021-04-11 16:53:00 -0300

    add map from int to roman numerals

---
+++ 2021/roman_numerals.ts
@@ -0,0 +1,22 @@
+export function solution(number: number): string {
+  // convert the number to a roman numeral
+  const romanNumeralsMap = {
+    1: 'I',
+    5: 'V',
+    10: 'X',
+    50: 'L',
+    100: 'C',
+    500: 'D',
+    1000: 'M',
+  }
+  return ''
+}
+
+/*
+ * In Roman numerals 1990 is rendered:
+ ** 1000=M,
+ ** 900=CM,
+ ** 90=XC; resulting in MCMXC.
+ ** 2008 is written as 2000=MM, 8=VIII; or MMVIII
+ ** 1666 uses each Roman symbol in descending order: MDCLXVI.
+ */
