5dd40272605c8b566fac7d56d84cd03e94f7da54
Lucas Jacques <lucas.jacs@gmail.com>
2021-04-12 09:50:36 -0300

    add second digit evalutation

--- 2021/roman_numerals.ts
+++ 2021/roman_numerals.ts
@@ -19,7 +19,7 @@ const fromDecimalToRoman = (decimal: number) => {
   })
 
   let result: string = ''
-  const decimalAsString = decimal.toString()
+  const decimalAsString = decimal.toString().split('').reverse().join('')
   for (let i = 0; i < decimalAsString.length; i++) {
     const element = parseInt(decimalAsString[i])
     result = transformDecimalUnitToRoman(element, i) + result
@@ -31,6 +31,9 @@ const transformDecimalUnitToRoman = (
   decimalUnit: number,
   i: number
 ): string => {
+  if (decimalUnit === 0) {
+    return ''
+  }
   if (i === 0) {
     for (
       let j = romanNumeralsMapSorted.length - 3;
@@ -57,17 +60,53 @@ const transformDecimalUnitToRoman = (
       }
     }
   }
+  if (i === 1) {
+    for (
+      let j = romanNumeralsMapSorted.length - 5,
+        decimalUnitParsed = decimalUnit * 10;
+      j <= romanNumeralsMapSorted.length - 3;
+      j++
+    ) {
+      const current = romanNumeralsMapSorted[j]
+      const subtractor =
+        romanNumeralsMapSorted[romanNumeralsMapSorted.length - 3]
+      if (decimalUnitParsed === current.decimal) {
+        return current.roman
+      }
+      if (decimalUnitParsed === current.decimal - subtractor.decimal) {
+        return `${subtractor.roman}${current.roman}`
+      }
+      if (decimalUnitParsed > current.decimal - subtractor.decimal) {
+        let remainder = decimalUnitParsed - current.decimal
+        let result = current.roman
+        while (remainder > 0) {
+          result += subtractor.roman
+          remainder -= subtractor.decimal
+        }
+        return result
+      }
+    }
+  }
   return ''
 }
-console.log('result: ', solution(1))
-console.log('result: ', solution(2))
-console.log('result: ', solution(3))
-console.log('result: ', solution(4))
-console.log('result: ', solution(5))
-console.log('result: ', solution(6))
-console.log('result: ', solution(7))
-console.log('result: ', solution(8))
-console.log('result: ', solution(9))
+console.log('result for 1: ', solution(1))
+console.log('result for 2: ', solution(2))
+console.log('result for 3: ', solution(3))
+console.log('result for 4: ', solution(4))
+console.log('result for 5: ', solution(5))
+console.log('result for 6: ', solution(6))
+console.log('result for 7: ', solution(7))
+console.log('result for 8: ', solution(8))
+console.log('result for 9: ', solution(9))
+console.log('result for 10: ', solution(10))
+console.log('result for 20: ', solution(20))
+console.log('result for 30: ', solution(30))
+console.log('result for 40: ', solution(40))
+console.log('result for 50: ', solution(50))
+console.log('result for 60: ', solution(60))
+console.log('result for 70: ', solution(70))
+console.log('result for 80: ', solution(80))
+console.log('result for 99: ', solution(99))
 
 /* Some infos:
  * In Roman numerals 1990 is rendered:
@@ -81,9 +120,12 @@ console.log('result: ', solution(9))
 /* Dev plan:
  *# map roman numerals - check
  *# transform from Decimal to Roman
- *## transform correctly: I to III - check
- *## transform correctly: 'IV' case
- *## refactor 'IV' adaptation to be used by 'X' as well
- *## refactor 'IV' adaptation to be used by 'C' but with 'L' as the subtractor instead of 'I' (make it an argument)
+ *## transform correctly: 1 to 3 - check
+ *## transform correctly: 'IV' case - check
+ *## refactor 'IV' adaptation to be used by 'X' as well - check
+ *## refactor 'IV' adaptation to be used by 'C' but with 'L' as the subtractor instead of 'I' (make it an argument) - check
+ *## transform correctly: 1 to 99 - check
  *## refactor 'IV' adaptation to be used by 'X' but with 'D' as the subtractor instead of 'L' (pass D as the argument)
+ *## transform correctly: 1 to 999
+ *## transform correctly: 1 to 3999
  */
