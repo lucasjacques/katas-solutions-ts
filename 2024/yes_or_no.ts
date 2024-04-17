94ef87428ca2cdf100425d14b2d94920634a7b6c
Lucas Jacques <lucas.jacs@gmail.com>
2024-04-17 20:58:00 +0100

    add another solution to yes_or_no

--- 2024/yes_or_no.ts
+++ 2024/yes_or_no.ts
@@ -1,5 +1,9 @@
 export const boolToWord = (bool: boolean): string => {
   return bool ? "Yes": "No";
 };
+console.log("boolToWord: ", boolToWord(true));
 
-console.log(boolToWord(false));
+// OBS: another nice solution found
+export const boolToWord2 = (bool: boolean): string => bool ? "Yes": "No";
+
+console.log("boolToWord2: ", boolToWord2(true));
