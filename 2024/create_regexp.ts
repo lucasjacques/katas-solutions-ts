22b237853f8e4b4348e7df1376b7faf2fb6f7c95
Lucas Jacques <lucas.jacs@gmail.com>
2024-06-20 23:26:22 -0300

    add solution to kata9: create_regex

--- 2024/create_regexp.ts
+++ 2024/create_regexp.ts
@@ -1,47 +1,51 @@
-//kata url: https://www.codewars.com/kata/5264d2b162488dc400000001/train/typescript
+//kata url: https://www.codewars.com/kata/52e1476c8147a7547a000811/train/typescript
 
 // assign your RegExp to REGEXP
-export const REGEXP = /(\d|[A-Z]|[a-z]){6}/;
+export const REGEXP = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)([A-Za-z\d]){6,}$/;
 
-// const tests = [
-//     { 
-//         value: '123456',
-//         expected: 'Hey wollef sroirraw'
-//     },
-//     { 
-//         value: 'a',
-//         expected: 'This is a test'
-//     },
-//     { 
-//         value: 'A',
-//         expected: 'This is rehtona test'
-//     },
-//     { 
-//         value: '1',
-//         expected: 'olleh dlrow'
-//     },
-//     { 
-//         value: '_',
-//         expected: 'false'
-//     },
-// ];
+const tests = [
+    { 
+        value: '123456',
+        expected: false
+    },
+    { 
+        value: '12345a',
+        expected: false
+    },
+    { 
+        value: '1234aA',
+        expected: true
+    },
+    { 
+        value: 'aaaa1A',
+        expected: true
+    },
+    { 
+        value: 'aaaa1A.',
+        expected: false
+    },
+];
 
-// function executeTests(testFn: (param: string) => string, tests: {value: string, expected: string}[]): boolean {
-//     const testsResult = tests.map((test) => {
-//         const testResultValue = testFn(test.value);
-//         const testResult = test.expected === testResultValue;
-//         const testMessage = testResult ? `TEST PASSED: Expected "${test.expected}" and got "${testResultValue}"` : `TEST FAILED: Expected "${test.expected}" and got "${testResultValue}"`;
+function execRegex(string: string) {
+    return REGEXP.test(string);
+}
+
+function executeTests(testFn: (param: string) => boolean, tests: {value: string, expected: boolean}[]): boolean {
+    const testsResult = tests.map((test) => {
+        const testResultValue = testFn(test.value);
+        const testResult = test.expected === testResultValue;
+        const testMessage = testResult ? `TEST PASSED: Expected "${test.expected}" and got "${testResultValue}"` : `TEST FAILED: Expected "${test.expected}" and got "${testResultValue}"`;
         
-//         console.log(testMessage);
+        console.log(testMessage);
         
-//         return testResult;
-//     }).reduce((previousTestResult, currentTestResult) => previousTestResult && currentTestResult)
+        return testResult;
+    }).reduce((previousTestResult, currentTestResult) => previousTestResult && currentTestResult)
     
-//     const testsMessage = testsResult ? `ALL TESTS PASSED @${testFn.name}! CONGRATULATIONS!\n\n` : `ONE OR MORE TESTS FAILED @${testFn.name}, PLEASE TRY AGAIN!\n\n`;
+    const testsMessage = testsResult ? `ALL TESTS PASSED @${testFn.name}! CONGRATULATIONS!\n\n` : `ONE OR MORE TESTS FAILED @${testFn.name}, PLEASE TRY AGAIN!\n\n`;
     
-//     console.log(testsMessage);
+    console.log(testsMessage);
     
-//     return testsResult;
-// }
+    return testsResult;
+}
 
-// executeTests(, tests);
+executeTests(execRegex, tests);
