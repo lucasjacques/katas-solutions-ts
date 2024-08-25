58c911e780f73ecd0c40c2c8126c6c49672f09f6
Lucas Jacques <lucas.jacs@gmail.com>
2024-08-25 11:53:13 -0300

    Kata13: alphanumeric - Add improved solution
    Removed unnecessary regex group (based on analysis of other solutions of this kata. Also used egex.test() instead of string.match() !== null to improve readibility

--- 2024/alphanumeric.ts
+++ 2024/alphanumeric.ts
@@ -22,6 +22,25 @@ export function alphanumeric(string: string): boolean {
     return string.match(regex) !== null
 }
 
+export function alphanumeric_improved(string: string): boolean {
+    /* 
+     * regex explained:
+     * / - regex start
+     ** ^ - detect start (to detect the start of the word to be filtered)
+     *- ( - group start - removed this group, it is unecessary
+     *** [ - match group
+     **** A-Za-z\d - all the characters to be permited: A to Z, a to z, digits
+     *** ] - match group
+     ** ) - group end - removed this group, it is unecessary
+     ** + - one or more
+     ** $ - detect end (to detect the end of the word to be filtered)
+     * / - regex end
+     */
+    
+    const regex = /^[A-Za-z\d]+$/;
+    return regex.test(string);
+}
+
 const tests = [
     { 
         value: 'Mazinkaiser',
@@ -60,3 +79,4 @@ function executeTests(testFn: (param: string) => boolean, tests: {value: string,
 }
 
 executeTests(alphanumeric, tests);
+executeTests(alphanumeric_improved, tests);
