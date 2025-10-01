a53e4eadc05678eb2ef11c78c219475291356a39
Lucas Jacques <lucas.jacs@gmail.com>
2025-10-01 15:43:46 -0300

    Add removeDuplicatesUsingFor complete solution

--- random/remove_duplicates.ts
+++ random/remove_duplicates.ts
@@ -1,24 +1,21 @@
-// const array = [1,2,3,3,4,5,6,6,6,7,8,9,10,10,1,2,3,2,12,11];
-const array = [1,2,3,3];
-
-const result: number[] = [];
-result.push(array[0]);
-
-for (let i = 0; i < array.length; i++) {
-  let tmp: number[] = []
-  for (let j = 0; j < result.length; j++){
-    // se o elemento estiver dentro do resultado
-    //   continue
-    if (result[j] === array[i]) {
-      continue;
+const removeDuplicatesUsingFor = () => {
+  const array = [1,2,3,3,4,5,6,6,6,7,8,9,10,10,1,2,3,2,12,11,12,12,11,1];
+  
+  const result: number[] = [];
+  result.push(array[0]);
+  
+  for (let i = 0; i < array.length; i++) {
+    let hasElement = false;
+    for (let j = 0; j < result.length; j++) {
+      if (result[j] === array[i]) {
+        hasElement = true;
+      }
     }
-    // se não estiver dentro do resultado
-    //   adicionar no resultado
-    else {
-      console.log('hey', result);
+    if (!hasElement) {
       result.push(array[i]);
     }
-  }
+  } 
+  console.log(result);
 }
 
-console.log(result);
+removeDuplicatesUsingFor();
